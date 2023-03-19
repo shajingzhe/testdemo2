6d4644f6686edb830e57de08afe707e42b8ea76b
shajingzhe <384673944@qq.com>
2023-03-19 14:01:23 +0800

    24

--- src/test/java/getIP.java
+++ src/test/java/getIP.java
@@ -5,7 +5,7 @@ import java.time.Instant;
 public class getIP {
     public static void main(String[] args) throws UnknownHostException {
         int a =1;
-        String m="21";
+        String m="24";
         System.out.println(InetAddress.getLocalHost());
     }
 }
