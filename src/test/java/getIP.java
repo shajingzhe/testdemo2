000dcc13e250c2b7a13d1f6d062efc92afb90bbd
shajingzhe <shajingzhe@homolo.com>
2023-03-29 09:39:46 +0800

    测试提交1

--- src/test/java/getIP.java
+++ src/test/java/getIP.java
@@ -5,7 +5,7 @@ import java.time.Instant;
 public class getIP {
     public static void main(String[] args) throws UnknownHostException {
         int a =2;
-        String m="24";
+        String m="241";
         System.out.println(InetAddress.getLocalHost());
     }
 }
