7174820912682b592bfeeb0f35d8c7e3f341e20c
shajingzhe <shajingzhe@homolo.com>
2023-03-30 13:45:41 +0800

    a4

--- src/test/java/getIP.java
+++ src/test/java/getIP.java
@@ -6,7 +6,7 @@ public class getIP {
     private static String  a="123";
 
     public static void main(String[] args) throws UnknownHostException {
-        String m="242";
+        String m="244";
         a="43";
         System.out.println(InetAddress.getLocalHost());
     }
