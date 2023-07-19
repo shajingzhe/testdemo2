a01df27151ec0526d550cd84abb079f3c2f15aa5
shajingzhe <shajingzhe@homolo.com>
2023-07-19 10:53:08 +0800

    661

--- src/test/java/NetChat.java
+++ src/test/java/NetChat.java
@@ -1,11 +1,16 @@
+import lombok.extern.slf4j.Slf4j;
+
 import java.net.InetAddress;
 
 /**
+ *  同wifi下ip的获取
  * @Author shajingzhe
  * @Date 2023/7/18 下午3:06
  */
+@Slf4j
 public class NetChat {
 	public static void checkHosts(String subnet){
+		long start = System.currentTimeMillis();
 		int timeout=1000;
 		for (int i=1;i<255;i++){
 			System.out.println("ip:"+i);
@@ -18,6 +23,8 @@ public class NetChat {
 				System.out.println( "None at " + host );
 			}
 		}
+		long end = System.currentTimeMillis();
+		log.info(String.format("Total Time：%d ms", end - start));
 	}
 	public static void main ( String[] args ) {
 		checkHosts("192.168.0");
