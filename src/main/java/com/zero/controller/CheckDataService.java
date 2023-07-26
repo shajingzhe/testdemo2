893ee88ba0009bd900b960f4d8b62d207d782709
shajingzhe <shajingzhe@homolo.com>
2023-07-26 16:53:58 +0800

    fix: 优化

--- src/main/java/com/zero/controller/CheckDataService.java
+++ src/main/java/com/zero/controller/CheckDataService.java
@@ -19,6 +19,8 @@ import java.util.List;
 import java.util.Map;
 
 /**
+ * 居村委员会双方数据对比
+ *
  * @Author shajingzhe
  * @Date 2023/7/21 下午3:36
  */
@@ -27,12 +29,18 @@ public class CheckDataService {
 	Map<String, List<String>> communityMap = new HashMap<>();
 	HashSet<String> communityHashSet = new HashSet<>();
 	List<Model> resultList = new ArrayList<>();
+	HashSet<String> errorNameSet = new HashSet<>();
+	String version = "2.3.3";
+
 
 	public static void main(String[] args) {
 	}
 
 	@Test
 	public void check() {
+		// 代码运行开始时间
+		Long startTime = System.currentTimeMillis();
+
 		initCommunityMap();
 
 		ExcelReader reader1 = ExcelUtil.getReader("/home/homolo/桌面/文档/测试文档/XLS 工作表.xls");
@@ -40,6 +48,7 @@ public class CheckDataService {
 		String resultValue;
 		for (Map<String, Object> map : readAll1) {
 			String juCunName = MapUtil.getStr(map, "居村社区");
+			String orginalZhengName = MapUtil.getStr(map, "街镇乡");
 			if (StringUtils.isBlank(juCunName)) {
 				Model model = new Model();
 				model.setOriginal("");
@@ -50,22 +59,38 @@ public class CheckDataService {
 			}
 			resultValue = "";
 			float levenshteinMax = 0;
+			String errorInfo = "";
+			String findZhengName = "";
 			String zhengName = MapUtil.getStr(map, "街镇乡");
 			List<String> communityNameList = communityMap.get(zhengName);
 			if (communityNameList == null) {
-				log.error("异常街镇乡数据:" + zhengName);
+				if (errorNameSet.add(zhengName)) {
+					log.error("异常街镇乡数据:" + zhengName);
+				}
+
+//				Iterator<Object> iterator = map.keySet().iterator();
+//				while (iterator.hasNext()) {
+//					Object key = iterator.next();
+//					System.out.println("key:" + key + ",vaule:" + map.get(key));
+//				}
+
 				//全表扫描
-				Iterator<List<String>> iterator = communityMap.values().iterator();
+				//Iterator<List<String>> iterator = communityMap.values().iterator();
+				Iterator<String> iterator = communityMap.keySet().iterator();
 				while (iterator.hasNext()) {
-					List<String> value = iterator.next();
+					String key = iterator.next();
+					List<String> value = communityMap.get(key);
 					for (String communityName : value) {
 						float levenshtein = LevenshteinUtils.levenshtein(juCunName, communityName);
 						if (levenshtein > levenshteinMax) {
 							levenshteinMax = levenshtein;
 							resultValue = communityName;
+							findZhengName = key;
 						}
 					}
 				}
+				errorInfo = "未找到对应街道，全表查找";
+
 
 			} else {
 				//局部扫描 todo s 抽出
@@ -81,34 +106,47 @@ public class CheckDataService {
 			model.setOriginal(juCunName);
 			model.setInfer(resultValue);
 			model.setSimilarity(levenshteinMax);
+			model.setOrginalZhengName(orginalZhengName);
+			if (!StringUtils.isBlank(errorInfo)) {
+				model.setErrorInfo(errorInfo);
+				model.setFindZhengName(findZhengName);
+			}
 			resultList.add(model);
 		}
 
+		System.out.println(StringUtils.join(errorNameSet, "、"));
 		LinkedHashMap<String, String> aliasMap = new LinkedHashMap<>();
 		aliasMap.put("original", "源数据");
 		aliasMap.put("infer", "推测数据");
 		aliasMap.put("similarity", "相似度");
+		aliasMap.put("errorInfo", "异常信息");
+		aliasMap.put("orginalZhengName", "原街道名称");
+		aliasMap.put("findZhengName", "查找街道名称");
 		List<Object> objectList = new ArrayList<>();
 		objectList.addAll(resultList);
-		FileUtils.generateXls(aliasMap, objectList, "社区信息推测(2.2).xls", "/home/homolo/桌面/文档/测试文档");
+		FileUtils.generateXls(aliasMap, objectList, "社区信息推测(" + version + ").xls", "/home/homolo/桌面/文档/测试文档");
+		// 代码运行结束时间
+		Long endTime = System.currentTimeMillis();
+		// 打印说明
+		System.out.println("程序执行时长"+(endTime-startTime));
 	}
 
 	/**
 	 * 初始化社区信息
 	 */
 	private void initCommunityMap() {
-		ExcelReader reader1 = ExcelUtil.getReader("/home/homolo/桌面/文档/测试文档/ 社区信息.xls");
+		ExcelReader reader1 = ExcelUtil.getReader("/home/homolo/桌面/文档/测试文档/居村委信息-社区云提供.xls");
 		List<Map<String, Object>> readAll1 = reader1.readAll();
 		for (Map<String, Object> map : readAll1) {
-			String zhengName = MapUtil.getStr(map, "街镇名称");
+			String zhengName = MapUtil.getStr(map, "街道");
 			if (communityHashSet.add(zhengName)) {
 				List<String> list = new ArrayList<>();
-				String juCunName = MapUtil.getStr(map, "居村名称");
+				String juCunName = MapUtil.getStr(map, "居村委");
 				list.add(juCunName);
 				communityMap.put(zhengName, list);
 			} else {
 				List<String> list = communityMap.get(zhengName);
-				String juCunName = MapUtil.getStr(map, "居村名称");
+				String juCunName = MapUtil.getStr(map, "居村委");
 				list.add(juCunName);
 			}
 		}
@@ -119,5 +157,8 @@ public class CheckDataService {
 		String original;
 		String infer;
 		float similarity;
+		String errorInfo;
+		String findZhengName;
+		String orginalZhengName;
 	}
 }
